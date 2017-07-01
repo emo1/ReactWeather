6eb582aa2c832e888bd9c961171e113bd4f33bba
= <=>
2017-07-01 23:47:36 +0300

    Style about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,9 +3,10 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
+      <h3 className="text-center">About</h3>
       <p>
-        Welcome to the about page
+        This is a weather application built on React.
+        I have built this for The Complete React Web App Developer Course.
       </p>
     </div>
   );
